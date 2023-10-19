c461af5618fa2920a457bfe859307cd483fc5f2f
GabrielGym <biel.ferreira12342@gmail.com>
2023-10-19 01:07:52 -0300

    feat: Header practically finished, just missing a few functions and adjustments

--- eletrica/src/components/Header.tsx
+++ eletrica/src/components/Header.tsx
@@ -1,5 +1,38 @@
-export const Header = () => (
-    <header>
-        
-    </header>
-)
+import { useState } from "react";
+import "../styles/Header.css"
+import { AiOutlineMenuUnfold } from 'react-icons/ai';
+
+export const Header = () => {
+   const [buttonMenu, setButtonMenu] = useState(false)
+
+    const openMenu = () => {
+        setButtonMenu(!buttonMenu)
+    }
+
+    return (
+        <header>
+            <div className="logo-header">
+                <h1>ELÉTRICA <span>J.SANTOS</span></h1>
+            </div>
+            <div className="open-infos-header-mobile">
+                <button onClick={() => openMenu()}><AiOutlineMenuUnfold/></button></div>
+            
+            {buttonMenu ? <div className="infos-header-mobile">
+                <button className="button-home">Home</button>
+                <button className="button-categoy">Categorias <img src="../assets/chevron-down.png" alt="Options categories"/></button>
+                <button className="button-about">Sobre</button>
+                <button className="button-location">Localização</button>
+                <button className="button-contact">Contato</button>
+           </div>: null}
+
+           <div className="infos-header">
+                <button className="button-home">Home</button>
+                <button className="button-categoy">Categorias <img src="../assets/chevron-down.png" alt="Options categories"/></button>
+                <button className="button-about">Sobre</button>
+                <button className="button-location">Localização</button>
+                <button className="button-contact">Contato</button>
+           </div>
+            
+        </header>
+    )
+} 
